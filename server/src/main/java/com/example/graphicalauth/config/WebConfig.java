90d87444a1345f4b2182f285b8ef265b6cf1a0d4
Saurabh Kumar <SAURABHKE4@GMAIL.COM>
2025-05-23 14:04:45 +0530

    WebConfig.java

--- server/src/main/java/com/example/graphicalauth/config/WebConfig.java
+++ server/src/main/java/com/example/graphicalauth/config/WebConfig.java
@@ -19,7 +19,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173" , "https://picpass-client.onrender.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
