34c0f40ce088e8ddb5036fb0d96290c8b5c0875b
Saurabh Kumar <SAURABHKE4@GMAIL.COM>
2025-05-24 11:23:28 +0530

    LoginRequest.java

--- server/src/main/java/com/example/graphicalauth/model/LoginRequest.java
+++ server/src/main/java/com/example/graphicalauth/model/LoginRequest.java
@@ -1,12 +1,10 @@
+// LoginRequest.java
 package com.example.graphicalauth.model;
 
 import lombok.Data;
-import java.util.List;
 
 @Data
 public class LoginRequest {
     private String username;
     private String password;
-    private List<Integer> selectedSegments;
-    private String ip;
 }
